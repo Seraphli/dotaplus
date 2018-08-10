25bee840d107329dd1ab663c1fa3baf9727e53fe
houxiao <houxiao424@163.com>
2018-08-11 01:10:30 +0800

    show all heros img...

--- gui/gui_select.py
+++ gui/gui_select.py
@@ -1,13 +1,15 @@
 import sys
+import warnings
+import os
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
 
+
 from gui.select import Ui_Form
 
-import warnings
-
+warnings.filterwarnings("ignore")
 
 class selectWindow(QWidget, Ui_Form):
     def __init__(self, parent=None):
@@ -23,10 +25,20 @@ class selectWindow(QWidget, Ui_Form):
         self.show()
 
     def set_btn_icon(self):
-        self.elder_titan_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/elder_titan.png'))
-        self.undying_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/undying.png'))
-        self.abaddon_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/abaddon.png'))
-        self.shredder_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/shredder.png'))
+        path = os.path.abspath('./miniheroes')
+        # self.elder_titan_Btn.setPixmap(QPixmap(path+'/ancient_apparition.png'))
+        # self.undying_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/undying.png'))
+        # self.abaddon_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/abaddon.png'))
+        # self.shredder_Btn.setPixmap(QPixmap('/Users/houxiao/workspace/python/dotaplus/gui/res/miniheroes/shredder.png'))
+        # print(self.omniknight_Btn.text())
+
+        layouts = [self.strLayout_1, self.strLayout_2, self.agiLayout_1, self.agiLayout_2, self.intLayout_1, self.intLayout_2]
+        for layout in layouts:
+            for i in range(layout.count()):
+                btn = layout.itemAt(i).widget()
+                if type(btn)==QLabel:
+                    btn.setPixmap(QPixmap(path+f'/{btn.text()}.png'))
+                    print(btn.objectName()[:-4])
 
 
 if __name__ == '__main__':
